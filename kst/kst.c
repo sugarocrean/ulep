6ea8e663cd6a5f17d5d94f39de5a41ffde53e50c
Haiyang Tan <haiyang.tan.dev@gmail.com>
2018-06-19 00:57:53 +0800

    1. GPL Licence 2. debug log

--- kst/kst.c
+++ kst/kst.c
@@ -1,12 +1,50 @@
+/* The idea for this came from Werner Almesberger's umlsim
+ * Copyright (C) 2004, Stephen Hemminger <shemminger@osdl.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kprobes.h>
 #include <linux/irq.h>
 #include <linux/proc_fs.h>
-#include <net/net_namespace.h>
 #include <linux/ktime.h>
 #include <linux/time.h>
+
+#include <net/net_namespace.h>
+
+
+MODULE_AUTHOR("Haiyang Tan <haiyang.tan.dev@gmail.com>");
+MODULE_DESCRIPTION("Process kernel mode time-consuming measurement");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.1");
+
+static char proc_name[NAME_MAX] = "loop4";
+module_param_string(proc, proc_name, NAME_MAX, S_IRUGO);
+MODULE_PARM_DESC(proc, " process to be measured; this module will report the"
+			" process's max kernel mode time");
+
+static int dbg_en __read_mostly;
+MODULE_PARM_DESC(dbg_en, "debug (0=disable debug, !0=enable)");
+module_param(dbg_en, int, 1);
+
+
+//#define dbg_log(fmt, ...) \
+//    printk(KERN_NOTICE, pr_fmt(fmt), ##__VA_ARGS__)
+#define dbg_log printk
+
 
 static raw_spinlock_t kst_lock;
 
@@ -24,7 +62,6 @@ static struct kst_meter kst_meters[KST_ENTRY_NUM];
 static int kst_meter_init(void)
 {
     raw_spin_lock_init(&kst_lock);
-
     return 0;
 }
 
@@ -88,11 +125,9 @@ static inline int kst_meter_remove(pid_t pid)
     return 0;
 }
 
-
-
 __visible unsigned int __irq_entry jsmp_apic_timer_interrupt(struct pt_regs *regs)
 {
-    if (!strcmp(current->comm, "loop4")) {
+    if (!strcmp(current->comm, proc_name)) {
         if (user_mode(regs)) {
             struct kst_meter *kst = kst_meter_lookup(current->pid);
             if (kst == NULL) {
@@ -101,10 +136,11 @@ __visible unsigned int __irq_entry jsmp_apic_timer_interrupt(struct pt_regs *reg
 
             if (kst) {
                 kst->u2k_time = ktime_get();
-                printk(KERN_INFO "%s: from user mode, now is %lu\n", __FUNCTION__, ktime_to_us(kst->u2k_time));
+                dbg_log("%s: from user mode, now is %lu\n", __FUNCTION__, ktime_to_us(kst->u2k_time));
             }
         } else {
-            dump_stack();
+           printk(KERN_NOTICE, "%s: irq occures in kernel mode", __FUNCTION__);
+           dump_stack();
         }
     }
 
@@ -116,15 +152,18 @@ __visible unsigned int __irq_entry jsmp_apic_timer_interrupt(struct pt_regs *reg
 static void kp_smp_apic_timer_interrupt_post(struct kprobe *p, struct pt_regs *regs,
                                 unsigned long flags)
 {
-    if (!strcmp(current->comm, "loop4")) {
+    if (!strcmp(current->comm, proc_name)) {
         struct kst_meter *kst = kst_meter_lookup(current->pid);
         if (kst) {
             ktime_t now = ktime_get();
             ktime_t delta = ktime_sub(now, kst->u2k_time);
             if (ktime_compare(delta, kst->max_time) > 0)
                 kst->max_time = delta;
-            printk(KERN_INFO "%s: pid is %d, now is %lu, diff is %lu, max is %lu\n", __FUNCTION__,
-                             current->pid, ktime_to_us(delta), ktime_to_us(kst->max_time), ktime_to_us(kst->max_time));
+            dbg_log("%s: pid is %d, now is %lu, diff is %lu, max is %lu\n", __FUNCTION__,
+                             current->pid,
+                             ktime_to_us(now),
+                             ktime_to_us(delta),
+                             ktime_to_us(kst->max_time));
         }
     }
 }
@@ -144,7 +183,7 @@ static struct kprobe kp_smp_apic_timer_interrupt = {
 
 __visible unsigned int __irq_entry jdo_IRQ(struct pt_regs *regs)
 {
-    if (!strcmp(current->comm, "loop4")) {
+    if (!strcmp(current->comm, proc_name)) {
         if (user_mode(regs)) {
             struct kst_meter *kst = kst_meter_lookup(current->pid);
             if (kst == NULL)
@@ -152,9 +191,11 @@ __visible unsigned int __irq_entry jdo_IRQ(struct pt_regs *regs)
 
             if (kst) {
                 kst->u2k_time = ktime_get();
-                printk(KERN_INFO "%s: from user mode, now is %lu\n", __FUNCTION__, ktime_to_us(kst->u2k_time));
+                dbg_log("%s: from user mode, now is %lu\n",
+                         __FUNCTION__, ktime_to_us(kst->u2k_time));
             }
         } else {
+            printk(KERN_NOTICE, "%s: irq occures in kernel mode", __FUNCTION__);
             dump_stack();
         }
     }
@@ -172,15 +213,18 @@ static struct jprobe jprobe_do_IRQ = {
 static void kp_do_IRQ_post(struct kprobe *p, struct pt_regs *regs,
                                 unsigned long flags)
 {
-    if (!strcmp(current->comm, "loop4")) {
+    if (!strcmp(current->comm, proc_name)) {
         struct kst_meter *kst = kst_meter_lookup(current->pid);
         if (kst) {
             ktime_t now = ktime_get();
             ktime_t delta = ktime_sub(now, kst->u2k_time);
             if (ktime_compare(delta, kst->max_time) > 0)
                 kst->max_time = delta;
-            printk(KERN_INFO "%s: pid is %d, now is %lu, diff is %lu, max is %lu\n", __FUNCTION__,
-                             current->pid, ktime_to_us(now), ktime_to_us(delta), ktime_to_us(kst->max_time));
+            dbg_log("%s: pid is %d, now is %lu, diff is %lu, max is %lu\n", __FUNCTION__,
+                     current->pid,
+                     ktime_to_us(now),
+                     ktime_to_us(delta),
+                     ktime_to_us(kst->max_time));
         }
     }
 }
@@ -190,15 +234,13 @@ static struct kprobe kp_do_IRQ = {
     .symbol_name = "do_IRQ",
 };
 
-
-
 void __noreturn jdo_exit(long code)
 {
-    if (!strcmp(current->comm, "loop4")) {
+    if (!strcmp(current->comm, proc_name)) {
         if (!kst_meter_remove(current->pid))
-            printk(KERN_INFO "%s: remove pid %d's record successfully\n", __FUNCTION__, current->pid);
+            dbg_log("%s: remove pid %d's record successfully\n", __FUNCTION__, current->pid);
         else
-            printk(KERN_INFO "%s: remove pid %d's record failed\n", __FUNCTION__, current->pid);
+            dbg_log("%s: remove pid %d's record failed\n", __FUNCTION__, current->pid);
     }
 
     /* Always end with a call to jprobe_return(). */
@@ -226,6 +268,8 @@ static int kst_proc_show(struct seq_file *m, void *v)
         }
     }
     raw_spin_unlock_irqrestore(&kst_lock, flags);
+
+    return 0;
 }
 
 static int kst_proc_open(struct inode *inode, struct file *file)
@@ -241,7 +285,7 @@ static const struct file_operations kst_proc_fops = {
 };
 
 
-static int __init jprobe_init(void)
+static int __init kst_mod_init(void)
 {
     int ret;
 
@@ -288,6 +332,7 @@ static int __init jprobe_init(void)
 
     proc_create("kst_info", 0, NULL, &kst_proc_fops);
 
+    pr_info("kst module is loaded\n");
     return 0;
 
 err1:
@@ -299,7 +344,7 @@ err3:
     return ret;
 }
 
-static void __exit jprobe_exit(void)
+static void __exit kst_mod_exit(void)
 {
     remove_proc_entry("kst_info", NULL);
     unregister_kprobe(&kp_smp_apic_timer_interrupt);
@@ -309,7 +354,7 @@ static void __exit jprobe_exit(void)
     unregister_jprobe(&jprobe_do_exit);
 }
 
-module_init(jprobe_init)
-module_exit(jprobe_exit)
+module_init(kst_mod_init)
+module_exit(kst_mod_exit)
 MODULE_LICENSE("GPL");
 
